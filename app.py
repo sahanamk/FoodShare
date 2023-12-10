6ae6b8ee29e58a1c8e089abf6206f8a2167ec02a
sahana <sahanamkotian@gmail.com>
2023-12-10 22:27:50 +0530

    backend frontend integration

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
 from flask import Flask, redirect, render_template, url_for
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 from flask_sqlalchemy import SQLAlchemy
-=======
->>>>>>> f2c050cf1fd6f723fb7e9dedcc4a43cff816b855
-=======
->>>>>>> f2c050cf1fd6f723fb7e9dedcc4a43cff816b855
+
 
 local_server=True
 app = Flask(__name__)
 app.secret_key="sahanasaanvi"
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 #app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql://username:password@localhost/databasename'
 app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql://root:@localhost/foodshare'
 db=SQLAlchemy(app)
@@ -27,16 +21,11 @@ class Hotel(db.Model):
     password=db.Column(db.String(20))
 
 
-=======
->>>>>>> f2c050cf1fd6f723fb7e9dedcc4a43cff816b855
-=======
->>>>>>> f2c050cf1fd6f723fb7e9dedcc4a43cff816b855
+
 @app.route("/")
 def login():
-    return render_template("login.html")
+    return render_template("restLogin.html") 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 @app.route("/test")
 def test():
     try:
@@ -47,9 +36,4 @@ def test():
         return f"database not connected {e}"
 
 
-
-=======
->>>>>>> f2c050cf1fd6f723fb7e9dedcc4a43cff816b855
-=======
->>>>>>> f2c050cf1fd6f723fb7e9dedcc4a43cff816b855
 app.run(debug=True)
