320e5341022158dc1e9f813e65aa52d780c3f800
sahana <sahanamkotian@gmail.com>
2023-12-10 19:19:59 +0530

    Home page

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, redirect, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+local_server=True
+app = Flask(__name__)
+app.secret_key="sahanasaanvi"
+
+#app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql://username:password@localhost/databasename'
+app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql://root:@localhost/foodshare'
+db=SQLAlchemy(app)
+
+class Hotel(db.Model):
+    hotelID=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(30))
+    email=db.Column(db.String(30))
+    phone=db.Column(db.Integer)
+    address=db.Column(db.String(100))
+    managerName=db.Column(db.String(20))
+    password=db.Column(db.String(20))
+
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/test")
+def test():
+    try:
+        a=Hotel.query.all()
+        print(a)
+        return "database connected"
+    except Exception as e:
+        return f"database not connected {e}"
+
+
+
+app.run(debug=True)
